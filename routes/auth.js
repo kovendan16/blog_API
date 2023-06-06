1480101d9487ced0efd3b8c639371c11c619b99c
kovendan16 <kovendan16@gmail.com>
2023-06-06 12:19:37 +0530

    newly aadded

--- routes/auth.js
+++ routes/auth.js
@@ -5,22 +5,18 @@ const bcrypt = require("bcrypt");
 //REGISTER
 router.post("/register", async (req, res) => {
   try {
-    
-    
-   const existname =  await User.findOne({name:req.body.username});
-   if (existname)return res.send("already name exist add numberic numbers like ex :user01");
+    const existname = await User.findOne({ name: req.body.username });
+    if (existname)
+      return res.send(
+        "already name exist add numberic numbers like ex :user01"
+      );
 
-   
-   const existemail =  await User.findOne({name:req.body.email});
-   if (existemail)return res.send("already register");
+    const existemail = await User.findOne({ name: req.body.email });
+    if (existemail) return res.send("already register");
 
-    
-    
-    
     const salt = await bcrypt.genSalt(10);
     const hashedPass = await bcrypt.hash(req.body.password, salt);
-    
-    
+
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
@@ -38,20 +34,18 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
-if   (!user) return res.status(400).json("Wrong credentials!");
+    if (!user) return res.status(400).json("Wrong credentials!");
 
-
-const email= await User.findOne({ email: req.body.email });
-if   (!email) return res.status(400).json("check you emai!");
-
+    const email = await User.findOne({ email: req.body.email });
+    if (!email) return res.status(400).json("check you emai!");
 
     const validated = await bcrypt.compare(req.body.password, user.password);
-  if   (!validated)  return  res.status(400).json("Wrong credentials!");
+    if (!validated) return res.status(400).json("Wrong credentials!");
 
     const { password, ...others } = user._doc;
- return   res.status(200).json(others);
+    return res.status(200).json(others);
   } catch (err) {
- return   res.status(500).json(err);
+    return res.status(500).json(err);
   }
 });
 
