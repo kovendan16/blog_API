59035afe15049a8602b4f31faf7f2104c5bee83d
kovendan16 <77107118+kovendan16@users.noreply.github.com>
2023-06-29 18:45:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -40,6 +40,6 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
-app.listen("5000||process.env.PORT", () => {
+app.listen("5000", () => {
   console.log("Backend is running. on 5000");
 });
